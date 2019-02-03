2425eb11bfce59f630058d467972d5104b6ffffd
John Solon <john.solon12@gmail.com>
2019-02-03 10:55:25 +0100

    Add show petition

--- client/src/views/ShowPetition.vue
+++ client/src/views/ShowPetition.vue
@@ -1,7 +1,28 @@
 <template>
   <v-container grid-list-xl>
-    <v-layout align-center justify-center row>
-      <h3>{{petition.title}}</h3>
+    <v-layout v-if="petition" align-center justify-center row>
+      <v-flex xs12>
+        <v-card class="mt-5">
+          <v-card-title>
+            <v-icon large left>fa-header</v-icon>
+            <span class="title font-weight-light">{{petition.title}}</span>
+            <v-spacer></v-spacer>
+            <p class="title font-weight-light">{{petition.signers.length}} signer(s)</p>
+          </v-card-title>
+          <v-card-text v-html="petition.description"></v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="success">
+              <v-icon class="mr-2">fa-pencil</v-icon>
+              <span>Sign</span>
+            </v-btn>
+            <v-btn>
+              <v-icon class="mr-2">fa-eraser</v-icon>
+              <span>Withdraw</span>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -14,7 +35,7 @@ import { Petition } from '@dpetition/lib';
 
 @Component
 export default class ShowPetition extends Vue {
-  petition: Petition;
+  public petition: Petition | undefined = undefined;
 
   @Action('list', { namespace: 'petition' }) private fetch!: () => void;
   @State('list', { namespace: 'petition' }) private petitions!: Petition[];
@@ -23,7 +44,7 @@ export default class ShowPetition extends Vue {
     if (this.petitions.length === 0) {
       await this.fetch();
     }
-    this.petition = this.petitions.find((p) => p.address === this.$route.params.address)
+    this.petition = this.petitions.find((p) => p.address === this.$route.params.address);
   }
 }
 </script>
